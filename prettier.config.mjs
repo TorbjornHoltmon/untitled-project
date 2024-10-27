d1e9c712c82b64b31deaa303bdb1e49aef320a2e
Torbjørn Holtmon <torbjornholtmon@gmail.com>
2024-10-27 23:11:35 +0100

    wip

---
+++ prettier.config.mjs
@@ -0,0 +1,14 @@
+// @ts-check
+
+/**
+ * @type {import('prettier').Config}
+ */
+export default {
+  printWidth: 120,
+  semi: false,
+  singleQuote: true,
+  arrowParens: 'always',
+  endOfLine: 'lf',
+  trailingComma: 'all',
+  pluginSearchDirs: false,
+}
